ab90a821decfb1b3cb93154105f23141711551a8
Victoria L Sumner <vicsumner@gmail.com>
2018-10-15 23:32:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  return kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop("Ralph")
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift("Milo")
+}
+
+function appendKitten(name){
+  return [...kittens,("Broom")]
+}
+
+function prependKitten(name){
+  return [("Arnold"), ...kittens]
+}
+
+function removeLastKitten(){
+  
+  return kittens
+}
